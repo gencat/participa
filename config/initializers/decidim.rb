8ac5a97f26328db5926f4d2a8cd5c5490cdd56c2
Isaac Massot <isaac.mg@coditramuntana.com>
2018-11-12 09:29:26 +0100

    initialize applicationand add missing locales

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -6,8 +6,8 @@ Decidim.configure do |config|
   config.authorization_handlers = [ExampleAuthorizationHandler]
 
   # Change these lines to set your preferred locales
-  config.default_locale = :en
-  config.available_locales = [:en, :ca, :es, :oc]
+  config.default_locale = :ca
+  config.available_locales = [:ca, :es, :oc]
 
   # Geocoder configuration
   config.geocoder = {
