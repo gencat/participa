4eba992df0f074ad573ba23eaef69d7d31cd665d
Isaac Massot <isaac.mg@coditramuntana.com>
2018-11-27 15:42:05 +0100

    update comands processes

--- decidim-process-extended/app/commands/decidim/participatory_processes/admin/update_participatory_process.rb
+++ decidim-process-extended/app/commands/decidim/participatory_processes/admin/update_participatory_process.rb
@@ -40,7 +40,13 @@ module Decidim
 
         def update_participatory_process
           @participatory_process.assign_attributes(attributes)
-          @participatory_process.save! if @participatory_process.valid?
+          if @participatory_process.valid?
+            @participatory_process.save!
+
+            Decidim.traceability.perform_action!(:update, @participatory_process, form.current_user) do
+              @participatory_process
+            end
+          end
         end
 
         def attributes
@@ -73,7 +79,7 @@ module Decidim
             # Participa added attributes
 
             #TODO: revisar lo de aplicar el downcase cuando no existe
-            #email: form.email.downcase, 
+            #email: form.email.downcase,
             email: form.email,
             show_home: form.show_home,
             decidim_department_id: form.department_id,
