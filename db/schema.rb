bca29fc6683537362456d4e355bfad021fc19711
Isaac Massot <isaac.mg@coditramuntana.com>
2018-12-13 14:52:57 +0100

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20181127134851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "decidim_accountability_results", id: :serial, force: :cascade do |t|
     t.jsonb "title"
