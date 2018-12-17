3797dc92a43bebafecadc04e8392fa4165717678
Isaac Massot <isaac.mg@coditramuntana.com>
2018-12-17 10:13:15 +0100

    fix newslter selectable query

--- decidim-selectable-news/app/jobs/decidim/admin/newsletter_job.rb
+++ decidim-selectable-news/app/jobs/decidim/admin/newsletter_job.rb
@@ -14,44 +14,36 @@ module Decidim
 
           @newsletter.update_attributes!(
             sent_at: Time.current,
-            total_recipients: recipients.count,
+            total_recipients: recipients(id).count,
             total_deliveries: 0
           )
         end
 
-        if id.nil?
-            recipients.find_each do |user|
-            NewsletterDeliveryJob.perform_later(user, @newsletter)
-            end
-        else
-            processrecipients(id).each do |user|
-            NewsletterDeliveryJob.perform_later(user, @newsletter)
-            end
+        recipients(id).find_each do |user|
+          NewsletterDeliveryJob.perform_later(user, @newsletter)
         end
       end
 
       private
 
-      def recipients
-        @recipients ||= User.where(organization: @newsletter.organization)
+      def recipients(id)
+        users = User.where(organization: @newsletter.organization)
                             .where.not(newsletter_notifications_at: nil, email: nil, confirmed_at: nil)
                             .not_deleted
+        if id.nil?
+          @recipients ||= users
+        else
+          @recipients ||= users.joins("left outer join decidim_follows fol
+                    on decidim_users.id = fol.decidim_user_id
+                    left outer join decidim_proposals_proposals pro
+                    on pro.id = fol.decidim_followable_id
+                    left outer join decidim_components fea
+                    on pro.decidim_component_id = fea.id
+                    left outer join decidim_participatory_processes p
+                    on p.id = fea.participatory_space_id
+                    where fol.decidim_followable_type = 'Decidim::Proposals::Proposal' and p.id = " + id).distinct
+        end
       end
-
-      def processrecipients(id)
-
-        @processrecipients = User.joins("left outer join decidim_follows fol
-                  on decidim_users.id = fol.decidim_user_id
-                  left outer join decidim_proposals_proposals pro
-                  on pro.id = fol.decidim_followable_id
-                  left outer join decidim_components fea
-                  on pro.decidim_component_id = fea.id
-                  left outer join decidim_participatory_processes p
-                  on p.id = fea.participatory_space_id
-                  where fol.decidim_followable_type = 'Decidim::Proposals::Proposal' and p.id = " + id).distinct
-
-      end
-
     end
   end
 end
