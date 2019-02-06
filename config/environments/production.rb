88e2a2f3ac6e21d2bf18a130e112eb2faeab55b4
Isaac Massot <isaac.mg@coditramuntana.com>
2019-02-06 14:38:18 +0100

    upgrade rails versions and i18n.fallbacks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = {
-    ca: [:ca, :es, :oc],
-    es: [:es, :ca, :oc],
-    oc: [:oc, :es, :ca],
-  }
+  config.i18n.fallbacks = true
 
   # Keeps the Last 5 log files which are rotated at every 10MB
   config.logger = Logger.new("./log/production.log", 5, 500.megabytes)
