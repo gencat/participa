73c1ba1f3fac94fe6da39aaf9822d3217b8f5cb0
ailobe <aitorlopezbeltran@gmail.com>
2019-02-13 13:35:02 +0100

    Deleted unused views

--- decidim-regulations/app/helpers/decidim/regulations/application_helper.rb
+++ decidim-regulations/app/helpers/decidim/regulations/application_helper.rb
@@ -1,6 +1,10 @@
 module Decidim
   module Regulations
     module ApplicationHelper
+      # Helper for participatory_processes show view
+      def is_regulation(process_id)
+        @is_regulation = (Decidim::ParticipatoryProcess.where(id: process_id).pluck(:decidim_participatory_process_group_id).first() == Rails.application.config.regulation)
+      end
     end
   end
 end
