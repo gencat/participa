55e5646d7fd21adaddee670fbcb0233dd46dea2e
ailobe <aitorlopezbeltran@gmail.com>
2019-02-13 16:39:35 +0100

    Move is_regulation helper

--- decidim-regulations/app/helpers/decidim/regulations/application_helper.rb
+++ decidim-regulations/app/helpers/decidim/regulations/application_helper.rb
@@ -1,10 +1,6 @@
 module Decidim
   module Regulations
     module ApplicationHelper
-      # Helper for participatory_processes show view
-      def is_regulation(process_id)
-        @is_regulation = (Decidim::ParticipatoryProcess.where(id: process_id).pluck(:decidim_participatory_process_group_id).first() == Rails.application.config.regulation)
-      end
     end
   end
 end
