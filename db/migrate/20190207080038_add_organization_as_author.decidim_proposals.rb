5dff40e891b83fb5095ccdb3af750b4e7b84acef
Isaac Massot <isaac.mg@coditramuntana.com>
2019-02-18 16:02:18 +0100

    fix migration

--- db/migrate/20190207080038_add_organization_as_author.decidim_proposals.rb
+++ db/migrate/20190207080038_add_organization_as_author.decidim_proposals.rb
@@ -4,7 +4,7 @@
 class AddOrganizationAsAuthor < ActiveRecord::Migration[5.2]
   def change
     default_organization = Decidim::Organization.first
-    Decidim::Proposals::Proposal.find_each.select do |proposal|
+    Decidim::Proposals::Proposal.find_each do |proposal|
       if proposal.coauthorships.count.zero?
         proposal.add_coauthor(default_organization)
       end
