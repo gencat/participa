89688b56556883b28a30583334ffad35d7560fdd
ailobe <aitorlopezbeltran@gmail.com>
2019-04-18 10:53:50 +0200

    Modify form and commands

--- decidim-process-extended/app/commands/decidim/participatory_processes/admin/update_participatory_process.rb
+++ decidim-process-extended/app/commands/decidim/participatory_processes/admin/update_participatory_process.rb
@@ -79,6 +79,10 @@ module Decidim
             announcement: form.announcement,
 
             # Participa added attributes
+            cost: form.cost,
+            has_summary_record: form.has_summary_record,
+            promoting_unit: form.promoting_unit,
+            facilitators: form.facilitators,
             email: form.email,
             show_home: form.show_home,
             decidim_department_id: form.department_id,
