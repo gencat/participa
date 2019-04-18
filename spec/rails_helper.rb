7bc59a89ef7b1932d8fddc27d83d09a638e071ae
ailobe <aitorlopezbeltran@gmail.com>
2019-04-18 18:14:45 +0200

    Modify comment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,13 +5,10 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-
+# Allow generating a dummy application to test your module
 require "decidim/dev"
-
 Decidim::Dev.dummy_app_path = File.expand_path(File.join(__dir__, ".."))
-
 require "decidim/dev/test/base_spec_helper"
-
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
