4e4c8c127d25fcde932873cad617d5b8b873d0a3
ailobe <aitorlopezbeltran@gmail.com>
2019-04-25 18:00:33 +0200

    Rename rake tasks

--- lib/socrata.rb
+++ lib/socrata.rb
@@ -8,11 +8,11 @@ module Socrata
     # Creates a file in the current directory.
     # Logs and Outputs the name of the file (timestamped).
     # Returns nothing.
-    def export(file_format)
+    def export(file_format, collection)
       log(:info, 'Exporting dataset...')
 
       exporter = Decidim::Exporters.find_exporter(file_format || 'CSV')
-      export_data = exporter.new(public_processes, serializer).export
+      export_data = exporter.new(collection, serializer).export
       file_name = export_data.filename('socrata_open_data')
       File.write(file_name, export_data.read)
 
@@ -24,9 +24,21 @@ module Socrata
     # Updates the remote Socrata dataset.
     # Logs and Outputs the HTTP response.
     # Returns nothing.
-    def publish
+    #
+    # - client: A SODA::Client instance. Needs to be authenticated.
+    #           https://www.rubydoc.info/github/socrata/soda-ruby/SODA/Client
+    #
+    # - payload: The body of the HTTP POST Request
+    #            Does not accept null values, so we remove them with #compact.
+    #
+    # - identifier: unique name for the dataset we want to access, as in:
+    #               https://soda.demo.socrata.com/dataset/Example-Dataset/identifier
+    def publish(collection)
       log(:info, 'Pushing data to Socrata...')
 
+      client = SODA::Client.new(Rails.application.secrets.soda)
+      dataset_identifier = Rails.application.secrets.soda[:dataset_identifier]
+      payload = collection.map { |resource| serializer.new(resource).serialize.compact }
       response = client.post(dataset_identifier, payload)
 
       log(:info, response.body)
@@ -43,34 +55,9 @@ module Socrata
       puts message
     end
 
-    # ParticipatoryProcesses that are published AND not private.
-    def collection
-      Decidim::ParticipatoryProcess.where(
-        organization: Decidim::Organization.first
-      ).published.public_spaces
-    end
-
     # Class used to transform ParticipatoryProcesses into data.
     def serializer
       Decidim::ParticipatoryProcesses::ParticipatoryProcessSerializer
-    end
-
-    # Returns a SODA::Client instance. Needs to be authenticated.
-    # https://www.rubydoc.info/github/socrata/soda-ruby/SODA/Client
-    def client
-      @client ||= SODA::Client.new(Rails.application.secrets.soda)
-    end
-
-    # Identifier for the dataset we want to access, as in:
-    # https://soda.demo.socrata.com/dataset/Example-Dataset/identifier
-    def dataset_identifier
-      Rails.application.secrets.soda[:dataset_identifier]
-    end
-
-    # Payload body of the HTTP POST Request
-    # Does not accept null values, so we remove them with Hash.compact().
-    def payload
-      collection.map { |process| serializer.new(process).serialize.compact }
     end
   end
 end
