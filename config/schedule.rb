34c6595283b87eea5ddcc2f865a387a92696727e
ailobe <aitorlopezbeltran@gmail.com>
2019-04-25 18:05:48 +0200

    Update shedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 env :PATH, ENV['PATH']
 
 every :sunday, at: '4:00 am' do
-  rake "decidim:delete_data_portability_files"
+  rake 'decidim:delete_data_portability_files'
 end
 
 every :sunday, at: '5:00 am' do
-  rake "decidim:open_data:export"
+  rake 'decidim:open_data:export'
 end
 
 every 1.day, at: '2:00 am' do
-  rake "decidim:metrics:all"
+  rake 'decidim:metrics:all'
 end
 
 every :sunday, at: '11:59 pm' do
-  rake "socrata:publish"
+  rake 'open_data:publish_to_socrata:participatory_processess'
 end
