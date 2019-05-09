c25589be1ec4b1bcca984244c78953b6f12bed3b
ailobe <aitorlopezbeltran@gmail.com>
2019-05-09 17:58:10 +0200

    Refactor selective_newsletter_form_decorator

--- app/decorators/decidim/admin/selective_newsletter_form_decorator.rb
+++ app/decorators/decidim/admin/selective_newsletter_form_decorator.rb
@@ -4,7 +4,8 @@ Decidim::Admin::SelectiveNewsletterForm.class_eval do
   def map_model(_newsletter)
     self.participatory_space_types = Decidim.participatory_space_manifests.map do |manifest|
       Decidim::Admin::SelectiveNewsletterParticipatorySpaceTypeForm.from_model(manifest: manifest)
-    end.unshift(additional_participatory_space_manifest)
+    end
+    participatory_space_types.unshift(additional_participatory_space_manifest)
   end
 
   private
