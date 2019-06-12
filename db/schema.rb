c1cd81b79e6db5760eac51b435a37cdc13d1919e
Isaac Massot <isaac.mg@coditramuntana.com>
2019-06-12 14:26:29 +0200

    force branch 0.18-stable and add last migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_151030) do
+ActiveRecord::Schema.define(version: 2019_06_12_120418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
