a3f4ec50a5d257c5b3c25598ee1752eb475bd1da
Isaac Massot <isaac.mg@coditramuntana.com>
2019-06-19 13:15:20 +0200

    fix when copying process/normative

--- decidim-process-extended/app/commands/decidim/participatory_processes/admin/copy_participatory_process.rb
+++ decidim-process-extended/app/commands/decidim/participatory_processes/admin/copy_participatory_process.rb
@@ -69,7 +69,7 @@ module Decidim
             facilitators: @participatory_process.facilitators,
             show_home: @participatory_process.show_home,
             email: @participatory_process.email,
-            decidim_type: @participatory_process.type
+            decidim_type_id: @participatory_process.decidim_type_id
           )
         end
 
