c77b140a636d265c3d5356620dd3f4811739d215
Oliver Valls <oliver.vh@coditramuntana.com>
2019-11-11 15:51:33 +0100

    Feature/avoid deletion of regulations group (#110)
    * [FEATURE] Decorate ParticipatoryProcessGroupsController#destroy to avoid deletion of required process group.
    
    * [DOC] Improve README with avoid_deletion_of_regulations_group decorator information.

---
+++ decidim-regulations/app/decorators/decidim/regulations/admin/avoid_deletion_of_regulations_group.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Regulations
+    module Admin
+      # To avoid admin users to delete special process groups
+      #
+      module AvoidDeletionOfRegulationsGroup
+        def destroy
+          forbidden_groups_ids = [
+            Rails.application.config.process.to_s,
+            Rails.application.config.regulation.to_s
+          ]
+          if forbidden_groups_ids.include?(params[:id])
+            flash[:notice] = "Protected ParticipatoryProcessGroup, can't be deleted."
+            redirect_to participatory_process_groups_path
+          else
+            super
+          end
+        end
+      end
+    end
+  end
+end
