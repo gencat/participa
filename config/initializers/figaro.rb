75376a1b7f83d2e722d618415bc53ec54d78d46d
Oliver Valls <oliver.vh@coditramuntana.com>
2019-11-25 15:16:15 +0100

    [CONF] Set expected vars per environment for Figaro.

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,8 +1,18 @@
 env= Rails.env
-unless env.development? or env.test?
+if env.development?
   Figaro.require_keys(
     "DB_DATABASE",
     "DB_PASSWORD",
     "DB_USERNAME"
+    )
+elsif env.test?
+  Figaro.require_keys(
+    "TEST_DB_DATABASE",
+    "TEST_DB_PASSWORD",
+    "TEST_DB_USERNAME"
+    )
+elsif env.preprod? or env.production?
+  Figaro.require_keys(
+    "DATABASE_URL",
     )
 end
