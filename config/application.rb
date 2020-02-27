79779a1bde50568c2619f432dbbcd0e720e9a94d
jarvisct <javier.jd@coditramuntana.com>
2020-02-27 11:14:49 +0100

    Set default timezone to Europe/Madrid

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Participa
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.available_locales = %w(en ca es oc)
+    config.time_zone = 'Europe/Madrid'
 
     # Processes group ids used to determine whether a process is a regulation or a process
     config.process    = 1
