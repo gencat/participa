448c93426791b08dea1b392c1b36e5ca0836d3ec
Ramon Costa <ramon.costa@gmail.com>
2020-09-17 15:11:08 +0200

    Clean code

--- app/decorators/decidim/debates/admin/debate_form_decorator.rb
+++ app/decorators/decidim/debates/admin/debate_form_decorator.rb
@@ -2,7 +2,6 @@
 
 Decidim::Debates::Admin::DebateForm.class_eval do
   def map_model(model)
-    byebug
     self.decidim_category_id = model.category.try(:id)
     self.start_time = model.start_time.strftime('%d/%m/%Y %H:%M') if model.start_time.present?
     self.end_time = model.end_time.strftime('%d/%m/%Y %H:%M') if model.end_time.present?
--- decidim-process-extended/app/decorators/decidim/participatory_processes/participatory_processes_controller_decorator.rb
+++ decidim-process-extended/app/decorators/decidim/participatory_processes/participatory_processes_controller_decorator.rb
@@ -2,25 +2,21 @@
 
 Decidim::ParticipatoryProcesses::ParticipatoryProcessesController.class_eval do
   def published_processes
-    byebug
-    @published_processes ||= OrganizationPublishedParticipatoryProcesses.new(current_organization, current_user).query.where(decidim_participatory_process_group_id: Rails.application.config.process)
+    @published_processes ||= Decidim::ParticipatoryProcesses::OrganizationPublishedParticipatoryProcesses.new(current_organization, current_user).query.where(decidim_participatory_process_group_id: Rails.application.config.process)
   end
 
   # This is customized because GENCAT don't Processes Groups on Index Page
   def collection
-    byebug
     @collection ||= participatory_processes
   end
   # This is customized because GENCAT don't Processes Groups on Index Page
 
   def filtered_processes
-    byebug
     search.results.where(decidim_participatory_process_group_id: Rails.application.config.process)
   end
 
   # This is customized because GENCAT
   def participatory_processes
-    byebug
     @participatory_processes ||= filtered_processes
   end
   # This is customized because GENCAT
