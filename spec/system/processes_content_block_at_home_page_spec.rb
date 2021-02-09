985d4a591635a6da084cb61cd7bc9bc212068f02
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-02-09 08:05:43 +0100

    Fix regulations should be filtered in processes content block (#210)

---
+++ spec/system/processes_content_block_at_home_page_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Processes ContentBlock at home page" do
+  let(:organization) { create :organization }
+
+  before do
+    switch_to_host(organization.host)
+  end
+
+  describe "with highlighted processes content block active" do
+    let!(:process) { create(:participatory_process, :promoted, :active, slug: 'process', organization: organization) }
+    let!(:regulation) { create(:participatory_process, :promoted, :active, slug: 'regulation', organization: organization, decidim_participatory_process_group_id: Rails.application.config.regulation) }
+    # let!(:highlighted_content_block) { create(:content_block, manifest_name: :highlighted_processes, organization: organization, scope_name: :homepage) }
+
+    before do
+      puts "proc: #{translated(process.title)} // reg: #{translated(regulation.title)}"
+      create(:content_block, manifest_name: :highlighted_processes, organization: organization, scope_name: :homepage)
+      # visit decidim.root_path
+    end
+
+    it "renders the process but not the regulation" do
+      visit decidim.root_path
+      expect(page).to have_content(translated(process.title))
+      expect(page).not_to have_content(translated(regulation.title))
+    end
+  end
+end
