521c9959ced852913b496d77cc200aabb8bcf412
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-02-19 13:33:07 +0100

    Remove decidim-process-extended/test as it is not being used

--- decidim-process-extended/test/decidim/process/extended_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class Decidim::Process::Extended::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Decidim::Process::Extended
-  end
-end
--- decidim-process-extended/test/dummy/config/routes.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do
-  mount Decidim::Process::Extended::Engine => "/decidim-process-extended"
-end
