a72ba52acad0b95e57affab21f88184742d1e4bb
spuigmitja <Sergi.Puigmitja@dxcfds.com>
2021-02-19 08:27:44 +0100

    fix query to filter space admins, not only admins, department_admins and user_admins

--- decidim-type/app/decorators/decidim/organization_decorator.rb
+++ decidim-type/app/decorators/decidim/organization_decorator.rb
@@ -2,4 +2,6 @@
 
 Decidim::Organization.class_eval do
   has_many :types, -> { order(name: :asc) }, foreign_key: "decidim_organization_id", class_name: "DecidimType", inverse_of: :organization
+  has_many :users_with_any_role, -> { where.not(roles: []).or(where('"decidim_users"."id" in (select "decidim_participatory_process_user_roles"."decidim_user_id" from "decidim_participatory_process_user_roles")' +
+    ' or "decidim_users"."id" in (select "decidim_assembly_user_roles"."decidim_user_id" from "decidim_assembly_user_roles")')) }, foreign_key: "decidim_organization_id", class_name: "Decidim::User"
 end
