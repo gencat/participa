061ba99fa37ee81401b4b9f30382a0ebb97f3343
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-02-19 13:45:16 +0100

    Remove decidim-regulations/test/ dir as it is not being used

--- decidim-regulations/test/decidim/regulations_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class Decidim::Regulations::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Decidim::Regulations
-  end
-end
--- decidim-regulations/test/dummy/config/routes.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do
-  mount Decidim::Regulations::Engine => "/decidim-regulations"
-end
