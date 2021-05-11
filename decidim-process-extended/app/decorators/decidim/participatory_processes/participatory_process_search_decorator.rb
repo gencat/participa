e7970217e92d5360724769a435a44aa1672e8f31
Laura Jaime <laura.jv@coditramuntana.com>
2021-05-11 08:57:43 +0200

    Changes in decidim-process-extended

--- decidim-process-extended/app/decorators/decidim/participatory_processes/participatory_process_search_decorator.rb
+++ decidim-process-extended/app/decorators/decidim/participatory_processes/participatory_process_search_decorator.rb
@@ -4,6 +4,7 @@ require_dependency "decidim/participatory_processes/participatory_process_search
 
 Decidim::ParticipatoryProcesses::ParticipatoryProcessSearch.class_eval do
   def initialize(options = {})
+    base_relation = options.has_key?(:base_relation) ? options.delete(:base_relation) : Decidim::ParticipatoryProcess.all
     super(Decidim::ParticipatoryProcess.where(decidim_participatory_process_group_id: Rails.application.config.process), options)
   end
 end
