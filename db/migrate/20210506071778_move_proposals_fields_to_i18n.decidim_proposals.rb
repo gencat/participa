1270b7a7d83c36937c0fa39ed491c087f8b702f0
Laura Jaime <laura.jv@coditramuntana.com>
2021-05-18 08:22:54 +0200

    Fix migration

--- db/migrate/20210506071778_move_proposals_fields_to_i18n.decidim_proposals.rb
+++ db/migrate/20210506071778_move_proposals_fields_to_i18n.decidim_proposals.rb
@@ -53,44 +53,41 @@ class MoveProposalsFieldsToI18n < ActiveRecord::Migration[5.2]
     add_column :decidim_proposals_proposals, :new_title, :string
     add_column :decidim_proposals_proposals, :new_body, :string
 
-    # Moved to IndexProposalsBeforeMoveFieldsToI18n migration and lib/tasks/proposals.rake
-    # reset_column_information
+    reset_column_information
 
-    # Decidim::Proposals::Proposal.find_each do |proposal|
-    #   proposal.new_title = proposal.title.values.first
-    #   proposal.new_body = proposal.body.values.first
+    Decidim::Proposals::Proposal.find_each do |proposal|
+      proposal.new_title = proposal.title.values.first
+      proposal.new_body = proposal.body.values.first
 
-    #   proposal.save!
-    # end
+      proposal.save!
+    end
 
-    # remove_indexs
+    remove_indexs
 
-    # remove_column :decidim_proposals_proposals, :title
-    # rename_column :decidim_proposals_proposals, :new_title, :title
-    # remove_column :decidim_proposals_proposals, :body
-    # rename_column :decidim_proposals_proposals, :new_body, :body
+    remove_column :decidim_proposals_proposals, :title
+    rename_column :decidim_proposals_proposals, :new_title, :title
+    remove_column :decidim_proposals_proposals, :body
+    rename_column :decidim_proposals_proposals, :new_body, :body
 
-    # create_indexs
+    create_indexs
 
-    # reset_column_information
+    reset_column_information
   end
 
-  # Moved to IndexProposalsBeforeMoveFieldsToI18n migration
+  def reset_column_information
+    Decidim::User.reset_column_information
+    Decidim::Coauthorship.reset_column_information
+    Decidim::Proposals::Proposal.reset_column_information
+    Decidim::Organization.reset_column_information
+  end
 
-  # def reset_column_information
-  #   Decidim::User.reset_column_information
-  #   Decidim::Coauthorship.reset_column_information
-  #   Decidim::Proposals::Proposal.reset_column_information
-  #   Decidim::Organization.reset_column_information
-  # end
+  def remove_indexs
+    remove_index :decidim_proposals_proposals, name: "decidim_proposals_proposal_title_search"
+    remove_index :decidim_proposals_proposals, name: "decidim_proposals_proposal_body_search"
+  end
 
-  # def remove_indexs
-  #   remove_index :decidim_proposals_proposals, name: "decidim_proposals_proposal_title_search"
-  #   remove_index :decidim_proposals_proposals, name: "decidim_proposals_proposal_body_search"
-  # end
-
-  # def create_indexs
-  #   execute "CREATE INDEX decidim_proposals_proposal_title_search ON decidim_proposals_proposals(md5(title::text))"
-  #   execute "CREATE INDEX decidim_proposals_proposal_body_search ON decidim_proposals_proposals(md5(body::text))"
-  # end
+  def create_indexs
+    execute "CREATE INDEX decidim_proposals_proposal_title_search ON decidim_proposals_proposals(md5(title::text))"
+    execute "CREATE INDEX decidim_proposals_proposal_body_search ON decidim_proposals_proposals(md5(body::text))"
+  end
 end
