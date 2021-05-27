f8153ab8bbcf8d28bd947a9f5fcda80f54629ad4
Laura Jaime <laura.jv@coditramuntana.com>
2021-05-27 08:03:58 +0200

    Enable erblint and comment override test

--- spec/features/overrides_and_customizations_spec.rb
+++ spec/features/overrides_and_customizations_spec.rb
@@ -3,16 +3,7 @@
 require "rails_helper"
 
 describe "Overrides and customizations" do
-  it "before Decidim v0.24.2" do
-    # ACTION -> re-enable erblint after Decidim v0.24
-    # Check the file .github/workflows/linters.yml to re-enable erb-lint
-
-    # ACTION -> depend upon gencat fork for CI migrations to work until Decidim 0.24
-    # When at v0.24 we can depend upon decidim:decidim again
-    # From gencat:decidim fork what we need is:
-    # commit 54b7e3713cc971dd27bd790eaf31c0b727f1c839 (HEAD -> release/0.22-stable, origin/release/0.22-stable)
-    #    Fix migration to avoid failing when generating test_app
-
-    expect(Decidim.version).to be <= "0.24.2"
-  end
+  # it "before Decidim v0.24" do
+  #   expect(Decidim.version).to be < "0.24"
+  # end
 end
