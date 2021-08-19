cfa31a2884213842e7e2e315d69dc5dc9090b110
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-08-19 13:35:57 +0200

    Rubocopify

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -13,7 +13,7 @@ describe "Homepage", type: :system do
       available_locales: [:ca, :en, :es]
     )
   end
-  let!(:hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :hero, settings: {"welcome_text_ca"=>"Benvinguda a Participa Gencat"} }
+  let!(:hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :hero, settings: { "welcome_text_ca"=>"Benvinguda a Participa Gencat" } }
   let!(:sub_hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :sub_hero }
 
   before do
