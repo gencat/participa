ad03661951226c27744ba84891b7a6b777e2e43d
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-08-23 08:52:08 +0200

    Add specs for the homepage (#258)
    * Add specs for the homepage
    
    * Remove CHANGELOG modification from pull_request_template
    
    * require rails_helper instead of spec_helper
    
    * Rubocopify

---
+++ spec/system/homepage_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Homepage", type: :system do
+  include Decidim::SanitizeHelper
+
+  let!(:organization) do
+    create(
+      :organization,
+      name: "Participa Gencat",
+      default_locale: :ca,
+      available_locales: [:ca, :en, :es]
+    )
+  end
+  let!(:hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :hero, settings: { "welcome_text_ca"=>"Benvinguda a Participa Gencat" } }
+  let!(:sub_hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :sub_hero }
+
+  before do
+    switch_to_host(organization.host)
+    visit decidim.root_path
+  end
+
+  it "loads and shows organization name and main blocks" do
+    visit decidim.root_path
+
+    expect(page).to have_content("Participa Gencat")
+    within "section.hero .hero__container" do
+      expect(page).to have_content("Benvinguda a Participa Gencat")
+    end
+    within "section.subhero:not(.footer__subhero)" do
+      subhero_msg= translated(organization.description).gsub(%r{</p>\s+<p>}, "<br><br>").gsub(%r{<p>(((?!</p>).)*)</p>}mi, "\\1")
+      expect(page).to have_content(subhero_msg)
+    end
+  end
+end
