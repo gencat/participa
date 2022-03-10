74723971c7ab8e17745cc57f789073d38c50c6d7
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-03-10 22:31:21 +0100

    Test when there is no process_group

--- decidim-process-extended/spec/system/admin/participatory_processes/admin_manages_participatory_processes_spec.rb
+++ decidim-process-extended/spec/system/admin/participatory_processes/admin_manages_participatory_processes_spec.rb
@@ -7,15 +7,35 @@ describe "Admin manages participatory processes", type: :system do
   let!(:type) { create(:type, organization: organization) }
   let!(:user) { create :user, :admin, :confirmed, organization: organization }
 
-  let!(:participatory_process_group) { create(:participatory_process_group, organization: organization, title: { en: "Normativa", es: "Normativa", ca: "Normativa" }) }
-  let!(:participatory_process) { create(:participatory_process, organization: organization, decidim_participatory_process_group_id: participatory_process_group.id) }
+  let!(:participatory_process_group) { nil }
+  let!(:participatory_process) { create(:participatory_process, organization: organization, decidim_participatory_process_group_id: participatory_process_group&.id) }
 
   before do
     switch_to_host(organization.host)
     login_as user, scope: :user
   end
 
+  describe "when there is no process group" do
+    context "and there is a published proposal component" do
+      let!(:proposal_component) do
+        create(:component, manifest_name: "proposals", participatory_space: participatory_process, published_at: Time.current)
+      end
+
+      it "don't show the proposal component alert" do
+        visit decidim_admin_participatory_processes.participatory_processes_path
+        click_link translated(participatory_process.title)
+        expect(page).not_to have_css(".alert")
+        expect(page).not_to have_text("Recordeu que heu d'activar el component propostes.")
+      end
+    end
+  end
+
   describe "when participatory process group is 'Normativa'" do
+    let!(:participatory_process_group) do
+      create(:participatory_process_group, organization: organization,
+                                           title: { en: "Normativa", es: "Normativa", ca: "Normativa" })
+    end
+
     context "and there is not a proposal component" do
       it "don't show the proposal component alert" do
         visit decidim_admin_participatory_processes.participatory_processes_path
