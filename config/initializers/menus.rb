24e7d3f6c1697af1fa7c4972ebc8e3b1b8ca2288
Laura Jaime <laura.jv@coditramuntana.com>
2023-02-14 08:39:24 +0100

    Rubocopify

--- config/initializers/menus.rb
+++ config/initializers/menus.rb
@@ -3,8 +3,8 @@
 Rails.application.config.to_prepare do
   Decidim.menu :menu do |menu|
     menu.item I18n.t("menu.decidims_finder", scope: "participagencat"),
-                Rails.application.routes.url_helpers.decidims_finder_page_path,
-                position: 6,
-                active: :inclusive
+              Rails.application.routes.url_helpers.decidims_finder_page_path,
+              position: 6,
+              active: :inclusive
   end
 end
