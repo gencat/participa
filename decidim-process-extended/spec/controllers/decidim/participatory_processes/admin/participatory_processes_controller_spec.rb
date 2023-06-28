1801250100adfc777aa5ad1cd82286a37209644b
NicoleSo13 <128817593+NicoleSo13@users.noreply.github.com>
2023-06-28 19:04:14 +0200

    Fix participatory_processes default images problem (#440)
    * Fix participatory_processes default images problem
    
    * Make test succeed & refactor
    
    * FIX: make webpack skip images compilation in process extender module
    
    * Fix Bundler version
    
    ---------
    
    Co-authored-by: Oliver Valls <199462+tramuntanal@users.noreply.github.com>

---
+++ decidim-process-extended/spec/controllers/decidim/participatory_processes/admin/participatory_processes_controller_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::ParticipatoryProcesses::Admin
+  describe ParticipatoryProcessesController, type: :controller do
+    routes { Decidim::ParticipatoryProcesses::AdminEngine.routes }
+    let(:organization) { create :organization, available_locales: [:ca, :en, :es] }
+    let(:current_user) { create :user, :admin, :confirmed, organization: organization }
+    let(:participatory_process) do
+      create(:participatory_process, organization: organization,
+                                     hero_image: nil, banner_image: nil)
+    end
+    let(:participatory_process_params) do
+      participatory_process.attributes.merge({
+                                               hero_image: "",
+                                               banner_image: ""
+                                             })
+    end
+
+    before do
+      request.env["decidim.current_organization"] = organization
+      sign_in current_user
+    end
+
+    describe "PATCH #update" do
+      context "when updating participatory process" do
+        it "assigns default images" do
+          expect(ParticipatoryProcessForm).to receive(:from_params).with(hash_including(id: participatory_process.id.to_s)).and_call_original
+          patch :update, params: { slug: participatory_process.id, participatory_process: participatory_process_params }
+
+          expect(response).to redirect_to(edit_participatory_process_path(participatory_process.slug))
+          expect(response).to have_http_status(:found)
+
+          expect(participatory_process.reload.hero_image.attachment&.blob&.filename).not_to eq(nil)
+          expect(participatory_process.reload.banner_image.attachment&.blob&.filename).not_to eq(nil)
+        end
+      end
+    end
+  end
+end
