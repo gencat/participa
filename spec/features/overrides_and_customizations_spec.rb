13455efb9fa207b50e94b217874d4c9b0f449faa
Laura Jaime <laura.jv@coditramuntana.com>
2023-08-24 11:23:01 +0200

    Fix translations

--- spec/features/overrides_and_customizations_spec.rb
+++ spec/features/overrides_and_customizations_spec.rb
@@ -3,8 +3,10 @@
 require "rails_helper"
 
 describe "Overrides and customizations" do
-  it "remove config/initializers/doorkeeper.rb after Decidim v0.28" do
-    # remove config/initializers/doorkeeper.rb after Decidim v0.28 as it is already configured in that version
+  it "remove config/initializers/doorkeeper.rb and remove translations in admin.yml and surveys.yml after Decidim v0.28" do
+    # remove config/initializers/doorkeeper.rb as it is already configured in that version
+    # xx_admin.yml and xx_surveys.yml have multiple translations that
+    # need to be removed if PR https://github.com/decidim/decidim/pull/11450/files gets merged into v0.28
     expect(Decidim.version).to be < "0.28"
   end
 end
