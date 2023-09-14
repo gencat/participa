08ca939103a45f1556b9be1ba0870da9d5ee13b0
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-09-14 18:33:09 +0200

    Get publish survey alert feature (#444)
    * Fix uncompatible rails/webpacker versions problem
    
    * Get publish survey alert feature
    
    * Rubocopify

--- spec/features/overrides_and_customizations_spec.rb
+++ spec/features/overrides_and_customizations_spec.rb
@@ -4,9 +4,10 @@ require "rails_helper"
 
 describe "Overrides and customizations" do
   it "remove config/initializers/doorkeeper.rb and remove translations in admin.yml and surveys.yml after Decidim v0.28" do
-    # remove config/initializers/doorkeeper.rb as it is already configured in that version
-    # xx_admin.yml and xx_surveys.yml have multiple translations that
+    # > remove config/initializers/doorkeeper.rb as it is already configured in that version
+    # > xx_admin.yml and xx_surveys.yml have multiple translations that
     # need to be removed if PR https://github.com/decidim/decidim/pull/11450/files gets merged into v0.28
+    # > check and remove config/initializers/webpacker.rb
     expect(Decidim.version).to be < "0.28"
   end
 end
