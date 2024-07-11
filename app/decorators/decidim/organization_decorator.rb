37af90924294adea7304cf3447bf0a8f9d15e868
Laura Jaime <laura.jv@coditramuntana.com>
2024-07-11 10:55:25 +0200

    Review app decorators

--- app/decorators/decidim/organization_decorator.rb
+++ app/decorators/decidim/organization_decorator.rb
@@ -6,6 +6,6 @@ Decidim::Organization.class_eval do
   has_many :users_with_any_role, lambda {
                                    where.not(roles: []).or(where('"decidim_users"."id" in (select "decidim_participatory_process_user_roles"."decidim_user_id" from "decidim_participatory_process_user_roles")' \
                                      ' or "decidim_users"."id" in (select "decidim_assembly_user_roles"."decidim_user_id" from "decidim_assembly_user_roles")'))
-                                 }, foreign_key: "decidim_organization_id", class_name: "Decidim::User"
+                                 }, foreign_key: "decidim_organization_id", class_name: "Decidim::User", dependent: :destroy
   # rubocop:enable Layout/LineLength
 end
