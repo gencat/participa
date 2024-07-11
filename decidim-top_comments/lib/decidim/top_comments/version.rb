23687d1063890a5263a535ccecfd2856f2f45b09
Laura Jaime <laura.jv@coditramuntana.com>
2024-07-11 15:35:08 +0200

    Update decidim-top_comments module

--- decidim-top_comments/lib/decidim/top_comments/version.rb
+++ decidim-top_comments/lib/decidim/top_comments/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-top_comments version.
   module TopComments
     VERSION= "0.26.2"
-    DECIDIM_VER= ">= 0.26"
+    DECIDIM_VER= ">= 0.27"
 
     def self.version
       VERSION
