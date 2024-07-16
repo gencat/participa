042661215bee396403bad05a2ecd65951144f84e
Laura Jaime <laura.jv@coditramuntana.com>
2024-07-16 14:45:45 +0200

    Add authors to proposals exportation

--- docs/HOW_TO_UPGRADE.md
+++ docs/HOW_TO_UPGRADE.md
@@ -186,7 +186,7 @@ These are custom modules and this is what you have to keep in mind when updating
 
 #### 6. Also, there are custom files in the application "participa.gencat.cat".
 
-  ##### 	Modified files:
+  ##### Modified files:
 
   With decorator pattern:
 
@@ -197,6 +197,10 @@ These are custom modules and this is what you have to keep in mind when updating
   * `decorators/decidim/participatory_space_context_decorator.rb`
     * Override to allow private space users to acces public view
     * probably removable from Decidim v0.24
+
+  * `decorators/lib/decidim/proposals/proposal_serializer_decorator.rb`
+    * Override to export proposal emails and names from authors
+    * probably removable from Decidim v0.28 (remember remove test too)
 
   * `lib/decidim/has_private_users.rb`
     * Override to allow private space users to acces public view
---
+++ spec/decorators/lib/decidim/proposals/proposal_serializer_decorator_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Decidim
+  module Proposals
+    describe ProposalSerializer do
+      subject do
+        described_class.new(proposal)
+      end
+
+      let!(:proposal) { create(:proposal, :accepted, body: body) }
+      let!(:category) { create(:category, participatory_space: component.participatory_space) }
+      let!(:scope) { create(:scope, organization: component.participatory_space.organization) }
+      let(:participatory_process) { component.participatory_space }
+      let(:component) { proposal.component }
+
+      let!(:meetings_component) { create(:component, manifest_name: "meetings", participatory_space: participatory_process) }
+      let(:meetings) { create_list(:meeting, 2, :published, component: meetings_component) }
+
+      let!(:proposals_component) { create(:component, manifest_name: "proposals", participatory_space: participatory_process) }
+      let(:other_proposals) { create_list(:proposal, 2, component: proposals_component) }
+      let(:body) { Decidim::Faker::Localized.localized { ::Faker::Lorem.sentences(number: 3).join("\n") } }
+
+      let(:expected_answer) do
+        answer = proposal.answer
+        Decidim.available_locales.each_with_object({}) do |locale, result|
+          result[locale.to_s] = if answer.is_a?(Hash)
+                                  answer[locale.to_s] || ""
+                                else
+                                  ""
+                                end
+        end
+      end
+
+      before do
+        proposal.update!(category: category)
+        proposal.update!(scope: scope)
+        proposal.link_resources(meetings, "proposals_from_meeting")
+        proposal.link_resources(other_proposals, "copied_from_component")
+      end
+
+      describe "#serialize" do
+        let(:serialized) { subject.serialize }
+
+        it "serializes the authors names" do
+          expect(serialized).to include(authors_names: proposal.authors.pluck(:name))
+        end
+
+        it "serializes the authors emails" do
+          expect(serialized).to include(authors_email: proposal.authors.pluck(:emails))
+        end
+      end
+    end
+  end
+end
