7dfe32d465c274c81527cb304139c6ed7c58325d
Laura Jaime <laura.jv@coditramuntana.com>
2024-07-18 10:08:58 +0200

    Refactor decorators and add missing field in form

--- decidim-process-extended/app/decorators/decidim/participatory_processes/admin/create_participatory_process_decorator.rb
+++ decidim-process-extended/app/decorators/decidim/participatory_processes/admin/create_participatory_process_decorator.rb
@@ -1,45 +1,16 @@
 # frozen_string_literal: true
 
 Decidim::ParticipatoryProcesses::Admin::CreateParticipatoryProcess.class_eval do
-  def attributes
-    {
-      organization: form.current_organization,
-      title: form.title,
-      subtitle: form.subtitle,
-      weight: form.weight,
-      slug: form.slug,
-      hashtag: form.hashtag,
-      description: form.description,
-      short_description: form.short_description,
-      hero_image: form.hero_image,
-      banner_image: form.banner_image,
-      promoted: form.promoted,
-      scopes_enabled: form.scopes_enabled,
-      scope: form.scope,
-      scope_type_max_depth: form.scope_type_max_depth,
-      private_space: form.private_space,
-      developer_group: form.developer_group,
-      local_area: form.local_area,
-      area: form.area,
-      target: form.target,
-      participatory_scope: form.participatory_scope,
-      participatory_structure: form.participatory_structure,
-      meta_scope: form.meta_scope,
-      start_date: form.start_date,
-      end_date: form.end_date,
-      participatory_process_group: form.participatory_process_group,
-      participatory_process_type: form.participatory_process_type,
-      show_metrics: form.show_metrics,
-      show_statistics: form.show_statistics,
-      announcement: form.announcement,
+  alias_method :original_attributes, :attributes
 
-      # Participa added attributes
+  def attributes
+    original_attributes.merge({
       cost: form.cost,
       has_summary_record: form.has_summary_record,
       promoting_unit: form.promoting_unit,
       facilitators: form.facilitators,
-      email: form.email.downcase,
+      email: form.email,
       decidim_type: form.type
-    }
+    })
   end
 end
