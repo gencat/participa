02d8c38d7230ec3da96a3bc5458b259c1722fe42
Ivan <ivan.mr@coditramuntana.com>
2024-09-25 14:32:02 +0200

    Refactor importer decorator spec to avoid rubocop issue stub methods

--- spec/decorators/decidim/admin/import/importer_decorator_spec.rb
+++ spec/decorators/decidim/admin/import/importer_decorator_spec.rb
@@ -19,42 +19,12 @@ describe Decidim::Admin::Import::Importer do
   end
   let(:participatory_process) { create :participatory_process, organization: organization }
   let(:current_component) { create :component, manifest_name: :proposals, participatory_space: participatory_process }
-
-  describe "#import!" do
-    context "when imported collection are proposals" do
-      let(:creator) { Decidim::Proposals::Import::ProposalCreator }
-      let(:blob) { upload_test_file(Decidim::Dev.asset("import_proposals.csv"), return_blob: true) }
-      let(:reader) { Decidim::Admin::Import::Readers::CSV }
-
-      it_behaves_like "proposal importer"
-
-      it "will execute notify_collection method" do
-        subject.prepare
-        expect(subject).to receive(:notify_collection)
-        subject.import!
-      end
-    end
-
-    context "when imported collection are answers proposals" do
-      let!(:proposal_component) { create(:proposal_component, organization: organization) }
-      let!(:proposal) { create(:proposal, component: proposal_component, id: 89_117) }
-      let(:creator) { Decidim::Proposals::Import::ProposalAnswerCreator }
-      let(:blob) { upload_test_file(Rails.root.join("lib", "assets", "import_answers.csv").to_s, return_blob: true) }
-      let(:reader) { Decidim::Admin::Import::Readers::CSV }
-
-      it "will execute notify_collection method" do
-        subject.prepare
-        expect(subject).to receive(:notify_collection)
-        subject.import!
-      end
-    end
-  end
+  let(:reader) { Decidim::Admin::Import::Readers::CSV }
 
   describe "#notify_collection" do
     context "when imported collection are proposals" do
       let(:creator) { Decidim::Proposals::Import::ProposalCreator }
       let(:blob) { upload_test_file(Decidim::Dev.asset("import_proposals.csv"), return_blob: true) }
-      let(:reader) { Decidim::Admin::Import::Readers::CSV }
 
       before do
         participatory_process.followers << follower
@@ -72,13 +42,12 @@ describe Decidim::Admin::Import::Importer do
     end
 
     context "when imported collection are answers proposals" do
-      let!(:proposal) { create(:proposal, :evaluating, component: current_component, id: 89_117) }
       let(:creator) { Decidim::Proposals::Import::ProposalAnswerCreator }
       let(:blob) { upload_test_file(Rails.root.join("lib", "assets", "import_answers.csv").to_s, return_blob: true) }
-      let(:reader) { Decidim::Admin::Import::Readers::CSV }
 
       before do
         participatory_process.followers << follower
+        create(:proposal, :evaluating, component: current_component, id: 89_117)
         ActionMailer::Base.deliveries.clear
         allow(ProposalsAnswersMailer).to receive(:notify_massive_import).and_call_original
       end
