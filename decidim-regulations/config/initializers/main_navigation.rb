463494a7353a9ee9d0880ba5309c613eac6e7ea4
Laura Jaime <laura.jv@coditramuntana.com>
2024-11-26 12:13:17 +0100

    Fix navbar and footer

--- decidim-regulations/config/initializers/main_navigation.rb
+++ decidim-regulations/config/initializers/main_navigation.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
+Rails.application.config.to_prepare do
+  Decidim.menu :menu do |menu|
+    menu.add_item :regulations,
+                  I18n.t("menu.regulations_static"),
+                  "/regulations",
+                  position: 2.1,
+                  active: :inclusive
+  end
 
-Decidim.menu :home_content_block_menu do |menu|
-  menu.add_item :regulations,
-                I18n.t("menu.regulations_static"),
-                "/regulations",
-                active: :inclusive
+  Decidim.menu :home_content_block_menu do |menu|
+    menu.add_item :regulations,
+                  I18n.t("menu.regulations_static"),
+                  "/regulations",
+                  position: 30,
+                  active: :inclusive
+  end
 end
