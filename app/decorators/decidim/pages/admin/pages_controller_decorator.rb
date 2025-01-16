1e1f06ff44518356e40bc29c4cf57dbb2c96017c
ivan <ivan.mr@coditramuntana.com>
2025-01-16 13:21:37 +0100

    Add test for page

--- app/decorators/decidim/pages/admin/pages_controller_decorator.rb
+++ app/decorators/decidim/pages/admin/pages_controller_decorator.rb
@@ -4,11 +4,6 @@ module Decidim::Pages::Admin::PagesControllerDecorator
   def self.decorate
     Decidim::Pages::Admin::PagesController.class_eval do
       helper_method :tab_panel_items
-
-      def edit
-        enforce_permission_to(:update, :page)
-        @form = form(::Decidim::Pages::Admin::PageForm).from_model(page)
-      end
     end
   end
 end
