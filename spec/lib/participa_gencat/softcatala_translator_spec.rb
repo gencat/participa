c6a62e7e71693f7552704efb82c86471f7ef657f
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2025-04-04 11:58:58 +0200

    Remove HTML before translating as Softcatala doesn't support it

--- spec/lib/participa_gencat/softcatala_translator_spec.rb
+++ spec/lib/participa_gencat/softcatala_translator_spec.rb
@@ -12,13 +12,14 @@ module ParticipaGencat
     let(:target_locale) { :ca }
     let(:field_name) { :title }
     let(:original_value) { "My title" }
+    let(:text_to_translate) { original_value }
     let(:translated_value) { "El meu títol" }
 
     let(:softcatala_client) { SoftcatalaClient.new }
 
     before do
       expect(SoftcatalaClient).to receive(:new).and_return(softcatala_client)
-      expect(softcatala_client).to receive(:translate).with(original_value, :en, :ca).and_return(translated_value)
+      expect(softcatala_client).to receive(:translate).with(text_to_translate, :en, :ca).and_return(translated_value)
     end
 
     it "delegates the translation to the softcatala_client" do
@@ -36,6 +37,15 @@ module ParticipaGencat
         )
 
       subject.translate
+    end
+
+    context "when there is HTML in the original text" do
+      let(:original_value) { "<h1>My title</h1>" }
+      let(:text_to_translate) { "My title" }
+
+      it "removes the HTML before sending the text to the client" do
+        subject.translate
+      end
     end
   end
 end
