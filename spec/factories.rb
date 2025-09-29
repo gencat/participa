cd8289373a629b86171af5e8069ec3c23d9fe99f
Laura Jaime <laura.jv@coditramuntana.com>
2025-09-29 16:47:54 +0200

    Remove duplicated factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,14 +4,3 @@ require "decidim/core/test/factories"
 require "decidim/participatory_processes/test/factories"
 require "decidim/proposals/test/factories"
 require "decidim/meetings/test/factories"
-
-FactoryBot.define do
-  factory :external_author, class: "Decidim::ExternalAuthor" do
-    transient do
-      skip_injection { false }
-    end
-
-    name { generate(:name) }
-    organization
-  end
-end
