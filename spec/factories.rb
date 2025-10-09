d625e6a7bd83c6125b54d5a55177672b9b982aba
Laura Jaime <laura.jv@coditramuntana.com>
2025-10-09 09:26:42 +0200

    Try to fix factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,15 @@ require "decidim/core/test/factories"
 require "decidim/participatory_processes/test/factories"
 require "decidim/proposals/test/factories"
 require "decidim/meetings/test/factories"
-require "factories/external_author.rb"
+require "decidim/pages/test/factories"
+
+FactoryBot.define do
+  factory :external_author, class: "Decidim::ExternalAuthor" do
+    transient do
+      skip_injection { false }
+    end
+
+    name { generate(:name) }
+    organization
+  end
+end
