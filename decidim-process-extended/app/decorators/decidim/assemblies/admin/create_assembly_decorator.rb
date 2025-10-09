198266abe54b5adda683804138858b9cd8765ba7
Laura Jaime <laura.jv@coditramuntana.com>
2025-10-09 08:17:05 +0200

    Rubocopify

--- decidim-process-extended/app/decorators/decidim/assemblies/admin/create_assembly_decorator.rb
+++ decidim-process-extended/app/decorators/decidim/assemblies/admin/create_assembly_decorator.rb
@@ -2,40 +2,42 @@
 
 module Decidim::Assemblies::Admin::CreateAssemblyDecorator
   def self.decorate
-    def run_after_hooks
-      add_admins_as_followers
-      link_participatory_processes
-      Decidim::ContentBlocksCreator.new(resource).create_default!
-      # process-extended customization
-      Decidim::User.org_admins_except_me(form.current_user).find_each do |user|
-        # Otherwise, it will be sent if realtime notifications are enabled
-        notify_admin(user) if send_participatory_space_news_email?(user)
+    Decidim::Assemblies::Admin::CreateAssembly.class_eval do
+      def run_after_hooks
+        add_admins_as_followers
+        link_participatory_processes
+        Decidim::ContentBlocksCreator.new(resource).create_default!
+        # process-extended customization
+        Decidim::User.org_admins_except_me(form.current_user).find_each do |user|
+          # Otherwise, it will be sent if realtime notifications are enabled
+          notify_admin(user) if send_participatory_space_news_email?(user)
+        end
+        # process-extended customization
       end
-      # process-extended customization
-    end
 
-    private
+      private
 
-    def notify_admin(user)
-      data = {
-        event: "decidim.events.participatory_space.created",
-        event_class: Decidim::SimpleParticipatorySpaceEvent,
-        resource: assembly,
-        affected_users: [user],
-        extra: {
-          author_name: form.current_user.name,
-          area: assembly.area&.name,
-          start_date: assembly.creation_date,
-          end_date: assembly.closing_date,
-          force_email: send_participatory_space_news_email?(user)
+      def notify_admin(user)
+        data = {
+          event: "decidim.events.participatory_space.created",
+          event_class: Decidim::SimpleParticipatorySpaceEvent,
+          resource: assembly,
+          affected_users: [user],
+          extra: {
+            author_name: form.current_user.name,
+            area: assembly.area&.name,
+            start_date: assembly.creation_date,
+            end_date: assembly.closing_date,
+            force_email: send_participatory_space_news_email?(user)
+          }
         }
-      }
 
-      Decidim::EventsManager.publish(**data)
-    end
+        Decidim::EventsManager.publish(**data)
+      end
 
-    def send_participatory_space_news_email?(user)
-      user.notification_settings.has_key?("participatory_space_news") && user.notification_settings["participatory_space_news"] == "1"
+      def send_participatory_space_news_email?(user)
+        user.notification_settings.has_key?("participatory_space_news") && user.notification_settings["participatory_space_news"] == "1"
+      end
     end
   end
 end
