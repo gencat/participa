354917cd9d43b1b5a4e71c9c368f957486ba9613
Laura Jaime <laura.jv@coditramuntana.com>
2025-10-27 15:41:25 +0100

    Remove require factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,7 +4,6 @@ require "decidim/core/test/factories"
 require "decidim/participatory_processes/test/factories"
 require "decidim/proposals/test/factories"
 require "decidim/meetings/test/factories"
-require "decidim/pages/test/factories"
 
 FactoryBot.define do
   factory :external_author, class: "Decidim::ExternalAuthor" do
