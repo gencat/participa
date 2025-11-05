5121e5b03fa60c2ea1ec995ae2a0e38a94957ec0
Laura Jaime <laura.jv@coditramuntana.com>
2025-11-05 08:55:27 +0100

    Rubocopify

--- decidim-process-extended/app/decorators/decidim/participatory_processes/admin/create_participatory_process_decorator.rb
+++ decidim-process-extended/app/decorators/decidim/participatory_processes/admin/create_participatory_process_decorator.rb
@@ -31,7 +31,7 @@ module Decidim::ParticipatoryProcesses::Admin::CreateParticipatoryProcessDecorat
         data = {
           event: "decidim.events.participatory_space.created",
           event_class: Decidim::SimpleParticipatorySpaceEvent,
-          resource: resource,
+          resource:,
           affected_users: [user],
           extra: {
             author_name: form.current_user.name,
