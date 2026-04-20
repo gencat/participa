dbe9c131a10b06972dff36305ced89d1bd6e4b0b
Ruben <ruben.bg@coditramuntana.com>
2026-04-20 10:04:05 +0200

    Patch wicked_pdf SprocketsEnvironment to avoid crash without Sprockets

---
+++ config/initializers/wicked_pdf_sprockets_fix.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Fix for wicked_pdf 2.8.x when Sprockets is not available (app uses Shakapacker).
+# The find_asset method falls through to SprocketsEnvironment which references
+# the Sprockets constant that doesn't exist, causing:
+#   uninitialized constant WickedPdf::WickedPdfHelper::Assets::SprocketsEnvironment::Sprockets
+#
+# See: https://github.com/mileszs/wicked_pdf/issues/1102
+# See: https://github.com/decidim/decidim/issues/14764
+#
+# This was fixed upstream in decidim >= 0.30 by removing wicked_pdf entirely.
+
+Rails.application.config.after_initialize do
+  WickedPdf::WickedPdfHelper::Assets::SprocketsEnvironment.class_eval do
+    def self.instance
+      return nil unless defined?(::Sprockets)
+
+      @instance ||= ::Sprockets::Railtie.build_environment(Rails.application)
+    end
+
+    def self.find_asset(*args)
+      return nil unless instance
+
+      instance.find_asset(*args)
+    end
+  end
+end
