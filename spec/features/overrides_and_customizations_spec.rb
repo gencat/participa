f6b611f4f9969f26dc9d5d4d540e9985be7d2e6b
Laura Jaime <laura.jv@coditramuntana.com>
2026-05-06 16:58:38 +0200

    Pin workbox-webpack-plugin package because error with crypto

--- spec/features/overrides_and_customizations_spec.rb
+++ spec/features/overrides_and_customizations_spec.rb
@@ -2,5 +2,10 @@
 
 require "rails_helper"
 
-# describe "Overrides and customizations" do
-# end
+describe "Overrides and customizations" do
+  # Please, check these anotations:
+  # - Remove pinned package "workbox-webpack-plugin" in package.json
+  it "Decidim version is >= 0.31" do
+    expect(Decidim::VERSION).to be >= "0.31.0"
+  end
+end
