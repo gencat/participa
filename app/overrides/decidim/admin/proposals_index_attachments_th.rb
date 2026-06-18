945968b7fa2838baf257baa58765a010632fdbb4
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2026-06-18 12:07:55 +0200

    Adapt latest developments from main to Decidim v0.30

--- app/overrides/decidim/admin/proposals_index_attachments_th.rb
+++ app/overrides/decidim/admin/proposals_index_attachments_th.rb
@@ -2,8 +2,9 @@
 
 # Inserts the "Attachments" column header after the "Valuators" <th>.
 Deface::Override.new(
-  virtual_path: "decidim/proposals/admin/proposals/index",
+  virtual_path: "decidim/proposals/admin/proposals/_proposals-thead",
   name: "proposals_admin_index_attachments_th",
+  original: "d819c75fcbdc48b6bab214a4cd282bba92ff400b",
   insert_after: "thead th:nth-last-of-type(3)",
   text: <<~EOHTML
     <% if current_component.settings.attachments_allowed? %>
